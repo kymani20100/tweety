d2c7e27b29fe9d8e5779b074acc2b7032540473b
Kymani <kymani20100@yahoo.com>
2018-03-26 17:12:25 +0000

    Addons to the follow and unfollow functionality...

--- core/classes/Follow.php
+++ core/classes/Follow.php
@@ -19,22 +19,22 @@ class Follow extends User {
             if($profileID != $user_id){
                 if($data['receiver'] == $profileID){
                     // Following btn
-                    echo "<button class='f-btn following-btn follow-btn' data-follow='".$profileID."'><i class='fa fa-rss'></i> Following</button>";
+                    return "<button class='f-btn following-btn follow-btn' data-follow='".$profileID."'><i class='fa fa-rss'></i> Following</button>";
                 }else{
                     // Follow btn
-                     echo "<button class='f-btn follow-btn' data-follow='".$profileID."'><i class='fa fa-rss'></i> Follow</button>";
+                     return "<button class='f-btn follow-btn' data-follow='".$profileID."'><i class='fa fa-rss'></i> Follow</button>";
                 }
             }else{
                 // Edit btn
-                echo "<button class='f-btn' onclick=location.href='profileEdit.php'><i class='fa fa-edit'></i> Edit Profile</button>";
+                return "<button class='f-btn' onclick=location.href='profileEdit.php'><i class='fa fa-edit'></i> Edit Profile</button>";
             }
         }else{
-            echo "<button class='f-btn' onclick=location.href='index.php'><i class='fa fa-user-plus'></i> Follow</button>";
+            return "<button class='f-btn' onclick=location.href='index.php'><i class='fa fa-user-plus'></i> Follow</button>";
         }
      }
 
      public function follow($followID, $user_id){
-        $this->create('follow', array('sender' => $user_id, 'receiver' => $followID, 'followOn' => date("Y:M:D H:i:s")));
+        $this->create('follow', array('sender' => $user_id, 'receiver' => $followID, 'followOn' => date("Y-M-D H-i-s")));
         $this->addFollowCount($followID, $user_id);
         $stmt = $this->pdo->prepare('SELECT * FROM `users` WHERE `user_id` = :followID');
         $stmt->execute(array("followID" => $followID));
@@ -59,6 +59,78 @@ class Follow extends User {
      public function removeFollowCount($followID, $user_id){
         $stmt = $this->pdo->prepare("UPDATE `users` SET `following` = `following` - 1 WHERE `user_id` = :user_id; UPDATE `users` SET `followers` = `followers` - 1 WHERE `user_id` = :followID");
         $stmt->execute(array("user_id" => $user_id, "followID" => $followID));
+     }
+
+     public function followingList($profileID, $user_id){
+        $stmt = $this->pdo->prepare("SELECT * FROM `users` LEFT JOIN `follow` ON `receiver` = `user_id` AND CASE WHEN `sender` = :user_id THEN `receiver` = `user_id` END WHERE `sender` IS NOT NULL");
+        $stmt->bindParam(":user_id", $profileID, PDO::PARAM_INT);
+        $stmt->execute();
+        $followings = $stmt->fetchAll(PDO::FETCH_OBJ); 
+        foreach($followings as $following){
+            echo '<div class="follow-unfollow-box">
+                    <div class="follow-unfollow-inner">
+                        <div class="follow-background">
+                            <img src="'.BASE_URL.$following->profileCover.'"/>
+                        </div>
+                        <div class="follow-person-button-img">
+                            <div class="follow-person-img"> 
+                                <img src="'.BASE_URL.$following->profileImage.'"/>
+                            </div>
+                            <div class="follow-person-button">
+                                 <!-- FOLLOW BUTTON -->
+                                 '.$this->followBtn($following->user_id, $user_id, $profileID).'
+                            </div>
+                        </div>
+                        <div class="follow-person-bio">
+                            <div class="follow-person-name">
+                                <a href="'.BASE_URL.$following->username.'">'.$following->screenName.'</a>
+                            </div>
+                            <div class="follow-person-tname">
+                                <a href="'.BASE_URL.$following->username.'">'.$following->username.'</a>
+                            </div>
+                            <div class="follow-person-dis">
+                               '.Tweet::getTweetLinks($following->bio).'
+                        </div>
+                    </div>
+                </div>
+            </div>';
+        }
+     }
+
+     public function followersList($profileID, $user_id){
+        $stmt = $this->pdo->prepare("SELECT * FROM `users` LEFT JOIN `follow` ON `sender` = `user_id` AND CASE WHEN `receiver` = :user_id THEN `sender` = `user_id` END WHERE `receiver` IS NOT NULL");
+        $stmt->bindParam(":user_id", $profileID, PDO::PARAM_INT);
+        $stmt->execute();
+        $followings = $stmt->fetchAll(PDO::FETCH_OBJ); 
+        foreach($followings as $following){
+            echo '<div class="follow-unfollow-box">
+                    <div class="follow-unfollow-inner">
+                        <div class="follow-background">
+                            <img src="'.BASE_URL.$following->profileCover.'"/>
+                        </div>
+                        <div class="follow-person-button-img">
+                            <div class="follow-person-img"> 
+                                <img src="'.BASE_URL.$following->profileImage.'"/>
+                            </div>
+                            <div class="follow-person-button">
+                                 <!-- FOLLOW BUTTON -->
+                                 '.$this->followBtn($following->user_id, $user_id, $profileID).'
+                            </div>
+                        </div>
+                        <div class="follow-person-bio">
+                            <div class="follow-person-name">
+                                <a href="'.BASE_URL.$following->username.'">'.$following->screenName.'</a>
+                            </div>
+                            <div class="follow-person-tname">
+                                <a href="'.BASE_URL.$following->username.'">'.$following->username.'</a>
+                            </div>
+                            <div class="follow-person-dis">
+                               '.Tweet::getTweetLinks($following->bio).'
+                        </div>
+                    </div>
+                </div>
+            </div>';
+        }
      }
 
 }
